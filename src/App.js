0958dc22cb9370feb6302e8f6a3395e1241d50c0
Kettengeschwindigkeit <666damned999@mail.ru>
2023-01-10 12:52:06 +0300

    07. What is Component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>one div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-          <li>js</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header />
+      <Technologies />
     </div>
   );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#s'>Home</a>
+      <a href='#s'>News Feed</a>
+      <a href='#s'>Messages</a>
+    </div>
+  )
+}
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
